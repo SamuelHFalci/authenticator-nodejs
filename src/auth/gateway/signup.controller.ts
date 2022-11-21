7661d82e95fef33b63f95b51609f6d7ea095c2cb
samuelhfalci <samuelhfalci@gmail.com>
2022-11-21 16:31:31 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/auth/gateway/signup.controller.ts
+++ src/auth/gateway/signup.controller.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from "../../@shared/errors/invalid-param-error";
 import { MissingParamError } from "../../@shared/errors/missing-param-error";
+import { ServerError } from "../../@shared/errors/server-error";
 import { IController } from "../../@shared/gateway/controller.interface";
 import { IHttpRequest, IHttpResponse } from "../../@shared/gateway/http";
 import { badRequest } from "../../@shared/helpers/http-helper";
@@ -8,20 +9,27 @@ import { IEmailValidator } from "../domain/validators/email-validator";
 export class SignUpController implements IController {
   constructor(private readonly emailValidator: IEmailValidator) {}
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFileds = [
-      "name",
-      "email",
-      "password",
-      "passwordConfirmation",
-    ];
-    for (const field of requiredFileds) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFileds = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
+      for (const field of requiredFileds) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
-    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isEmailValid) {
-      return badRequest(new InvalidParamError("email"));
+      const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isEmailValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
   }
 }
