4ca7ad27715d720c9c173dbb83dcf1d9d10fefe0
samuelhfalci <samuelhfalci@gmail.com>
2022-11-21 19:15:41 -0300

    feat: ensure DbAddAccount calls Encrypter with correct password

---
+++ src/auth/application/usecases/db-add-account.usecase.ts
@@ -0,0 +1,14 @@
+import { IEncrypter } from '../../domain/interfaces/encrypter.interface'
+import { IAccountModel } from '../../domain/models/account'
+import {
+  IAddAccountModel,
+  IAddAccountUsecase
+} from '../../domain/usecases/add-account.usecase.interface'
+
+export default class DbAddAccountUsecase implements IAddAccountUsecase {
+  constructor(private readonly encrypter: IEncrypter) {}
+  async add(account: IAddAccountModel): Promise<IAccountModel> {
+    this.encrypter.encrypt(account.password)
+    return new Promise((resolve) => resolve(null))
+  }
+}
