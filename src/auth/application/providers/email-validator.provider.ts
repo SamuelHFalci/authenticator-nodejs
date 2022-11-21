b7e444fd61518b7869d40b312ffe871e858f6441
samuelhfalci <samuelhfalci@gmail.com>
2022-11-21 18:52:24 -0300

    feat: ensure EmailValidatorProvider returns true if validator returns true

--- src/auth/application/providers/email-validator.provider.ts
+++ src/auth/application/providers/email-validator.provider.ts
@@ -1,7 +1,7 @@
 import { IEmailValidator } from "../../domain/validators/email-validator";
-
+import validator from "validator";
 export class EmailValidatorProvider implements IEmailValidator {
   isValid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
