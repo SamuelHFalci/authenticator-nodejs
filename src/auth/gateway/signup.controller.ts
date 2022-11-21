3d309620fae209a09087120832c77bca6e0ad230
samuelhfalci <samuelhfalci@gmail.com>
2022-11-21 15:59:01 -0300

    feat: ensure SignUpController returns 400 if an invalid email is provided

--- src/auth/gateway/signup.controller.ts
+++ src/auth/gateway/signup.controller.ts
@@ -1,9 +1,12 @@
+import { InvalidParamError } from "../../@shared/errors/invalid-param-error";
 import { MissingParamError } from "../../@shared/errors/missing-param-error";
 import { IController } from "../../@shared/gateway/controller.interface";
 import { IHttpRequest, IHttpResponse } from "../../@shared/gateway/http";
 import { badRequest } from "../../@shared/helpers/http-helper";
+import { IEmailValidator } from "../domain/validators/email-validator";
 
 export class SignUpController implements IController {
+  constructor(private readonly emailValidator: IEmailValidator) {}
   handle(httpRequest: IHttpRequest): IHttpResponse {
     const requiredFileds = [
       "name",
@@ -15,6 +18,10 @@ export class SignUpController implements IController {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+    const isEmailValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isEmailValid) {
+      return badRequest(new InvalidParamError("email"));
     }
   }
 }
