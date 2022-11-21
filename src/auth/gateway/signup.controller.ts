5bd126aa4f5c75238aa1121d214fdafec7b951a3
samuelhfalci <samuelhfalci@gmail.com>
2022-11-21 17:54:26 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/auth/gateway/signup.controller.ts
+++ src/auth/gateway/signup.controller.ts
@@ -2,10 +2,14 @@ import { InvalidParamError, MissingParamError } from "../../@shared/errors";
 import { IController } from "../../@shared/gateway/controller.interface";
 import { IHttpRequest, IHttpResponse } from "../../@shared/gateway/http";
 import { badRequest, serverError } from "../../@shared/helpers/http-helper";
+import { IAddAccountUsecase } from "../domain/usecases/add-account.usecase.interface";
 import { IEmailValidator } from "../domain/validators/email-validator";
 
 export class SignUpController implements IController {
-  constructor(private readonly emailValidator: IEmailValidator) {}
+  constructor(
+    private readonly emailValidator: IEmailValidator,
+    private readonly addAccountUsecase: IAddAccountUsecase
+  ) {}
   handle(httpRequest: IHttpRequest): IHttpResponse {
     try {
       const requiredFileds = [
@@ -19,7 +23,7 @@ export class SignUpController implements IController {
           return badRequest(new MissingParamError(field));
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body;
+      const { email, password, passwordConfirmation, name } = httpRequest.body;
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError("passwordConfirmation"));
       }
@@ -27,6 +31,11 @@ export class SignUpController implements IController {
       if (!isEmailValid) {
         return badRequest(new InvalidParamError("email"));
       }
+      this.addAccountUsecase.add({
+        name,
+        email,
+        password,
+      });
     } catch (error) {
       return serverError();
     }
