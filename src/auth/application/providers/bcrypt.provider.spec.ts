cdfd2d64ecdeab2dabdafcf9c0f788c2a8299c15
samuelhfalci <samuelhfalci@gmail.com>
2022-11-22 08:42:53 -0300

    test: ensure BcryptProvider calls bcrypt with correct values

---
+++ src/auth/application/providers/bcrypt.provider.spec.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt'
+import { BcryptProvider } from './bcrypt.provider'
+describe('Bcrypt Provider', () => {
+  test('should call bcrypt with correct value', async () => {
+    const sut = new BcryptProvider()
+    const hashSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.encrypt('any_value')
+    expect(hashSpy).toHaveBeenCalledWith('any_value', 12)
+  })
+})
