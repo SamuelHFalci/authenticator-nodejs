1cb353c1571c6322ea2165e20e68258ce28b29cc
samuelhfalci <samuelhfalci@gmail.com>
2022-11-22 08:42:44 -0300

    feat: ensure BcryptProvider calls bcrypt with correct values

---
+++ src/auth/application/providers/bcrypt.provider.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt'
+import { IEncrypter } from '../../domain/interfaces/encrypter.interface'
+export class BcryptProvider implements IEncrypter {
+  constructor(private readonly salt: number = 12) {}
+
+  async encrypt(value: string): Promise<string> {
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
+    return new Promise((resolve) => resolve(''))
+  }
+}
